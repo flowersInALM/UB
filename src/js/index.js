8cfd9284cd20c11ab1b11503e1004c1e8cfcef24
axmiao <18610834353@163.com>
2017-09-25 16:15:59 +0800

    show-page

--- src/js/index.js
+++ src/js/index.js
@@ -75,4 +75,61 @@ $(function(){
 		$('body,html').animate({scrollTop:0},1000);
 		return false;
 	});
+
+	// 图片尺寸自适应；
+	var widtu = $('.focus').width()*0.92;
+	var widtu2 = $('.focus2').width()*0.608;
+	$(".focus .childe").width(widtu);
+	$('.focus2 .childe').width(widtu2);
+	window.addEventListener('resize',function(){
+		widtu = $('.focus').width() * 0.92;
+		widtu2 = $('.focus2').width() * 0.608;
+		$('.focus .childe').width(widtu);
+		$('.focus2 .childe').width(widtu2);
+	})
+	// 翻页动作;
+	$('.focus .next').on('click',function(){
+		var length = $('.focus .childe').length;
+		var now = $('.focus .now').index();
+		if(now < length - 1){
+			now ++;
+			$('.focus .nav').find('i').eq(now).addClass('now').siblings().removeClass('now');
+			$(".focus .scroll").animate({
+				marginLeft: '-' + now * widtu + 'px'
+			})
+		}
+	})
+	$('.focus .prev').on('click',function(){
+		var length = $('.focus .childe').length;
+		var now = $('.focus .now').index();
+		if(now > 0){
+			now --;
+			$(".focus .nav").find('i').eq(now).addClass('now').siblings().removeClass('now');
+			$('.focus .scroll').animate({
+				marginLeft: '-' + now * widtu + 'px'
+			})
+		}
+	});
+	$('.focus2 .next').on('click',function(){
+		var length = $('.focus2 .childe').length;
+		var now = $('.focus2 .now').index();
+		if(now < length - 1){
+			now ++;
+			$('.focus2 .nav').find('i').eq(now).addClass('now').siblings().removeClass('now');
+			$(".focus2 .scroll").animate({
+				marginLeft: '-' + now * widtu2 + 'px'
+			})
+		}
+	})
+	$('.focus2 .prev').on('click',function(){
+		var length = $('.focus2 .childe').length;
+		var now = $('.focus2 .now').index();
+		if(now > 0){
+			now --;
+			$(".focus2 .nav").find('i').eq(now).addClass('now').siblings().removeClass('now');
+			$('.focus2 .scroll').animate({
+				marginLeft: '-' + now * widtu2 + 'px'
+			})
+		}
+	});
 });

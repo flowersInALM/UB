ffe53b955a6b10f93c89b07aa39af1bf3041a501
axmiao <18610834353@163.com>
2017-10-10 15:17:28 +0800

    修改详情页适配及面向对象封装翻页函数

--- src/js/index.js
+++ src/js/index.js
@@ -76,60 +76,98 @@ $(function(){
 		return false;
 	});
 
-	// 图片尺寸自适应；
-	var widtu = $('.focus').width()*0.92;
-	var widtu2 = $('.focus2').width()*0.608;
-	$(".focus .childe").width(widtu);
-	$('.focus2 .childe').width(widtu2);
-	window.addEventListener('resize',function(){
-		widtu = $('.focus').width() * 0.92;
-		widtu2 = $('.focus2').width() * 0.608;
-		$('.focus .childe').width(widtu);
-		$('.focus2 .childe').width(widtu2);
-	})
-	// 翻页动作;
-	$('.focus .next').on('click',function(){
-		var length = $('.focus .childe').length;
-		var now = $('.focus .now').index();
-		if(now < length - 1){
-			now ++;
-			$('.focus .nav').find('i').eq(now).addClass('now').siblings().removeClass('now');
-			$(".focus .scroll").animate({
-				marginLeft: '-' + now * widtu + 'px'
-			})
-		}
-	})
-	$('.focus .prev').on('click',function(){
-		var length = $('.focus .childe').length;
-		var now = $('.focus .now').index();
-		if(now > 0){
-			now --;
-			$(".focus .nav").find('i').eq(now).addClass('now').siblings().removeClass('now');
-			$('.focus .scroll').animate({
-				marginLeft: '-' + now * widtu + 'px'
-			})
-		}
-	});
-	$('.focus2 .next').on('click',function(){
-		var length = $('.focus2 .childe').length;
-		var now = $('.focus2 .now').index();
-		if(now < length - 1){
-			now ++;
-			$('.focus2 .nav').find('i').eq(now).addClass('now').siblings().removeClass('now');
-			$(".focus2 .scroll").animate({
-				marginLeft: '-' + now * widtu2 + 'px'
-			})
-		}
-	})
-	$('.focus2 .prev').on('click',function(){
-		var length = $('.focus2 .childe').length;
-		var now = $('.focus2 .now').index();
-		if(now > 0){
-			now --;
-			$(".focus2 .nav").find('i').eq(now).addClass('now').siblings().removeClass('now');
-			$('.focus2 .scroll').animate({
-				marginLeft: '-' + now * widtu2 + 'px'
-			})
-		}
-	});
 });
+// 暴露对象接口给window；
+(function(window,document){
+	var FunPage = function(targetDom,options){
+		// 判断实例化方式；
+		if(!(this instanceof FunPage)) return new FunPage(targetDom,options);
+		// 初始参数设置；
+		this.options = options;
+		this.scaleW = this.options.scaleW ? this.options.scaleW : 1;
+		if((typeof targetDom) == 'string'){
+			this.targetDom = document.querySelector(targetDom);
+		}else{
+			this.targetDom = targetDom;
+		}
+		
+		this.options.childDom = this.options.childDom ? this.options.childDom: '.childe';
+		if((typeof this.options.childDom) == 'string'){
+			this.childDom = this.targetDom.querySelectorAll(this.options.childDom);
+		}else{
+			this.childDom = this.options.childDom;
+		}
+		// 翻页键；
+		this.prevBtn = this.targetDom.querySelector('.prev');
+		this.nextBtn = this.targetDom.querySelector('.next');
+
+		this.parentWid = this.targetDom.offsetWidth;
+		this.childWid = parseFloat(this.parentWid) * parseFloat(this.scaleW);
+		for(var i= 0; i < this.childDom.length; i ++){
+			this.childDom[i].style.width = this.childWid + 'px';
+		}
+		// 调用init方法；
+		this.init();
+	};
+
+	FunPage.prototype = {
+		init : function(){
+			this.event();
+		},
+		event : function(){
+			var _this = this;
+
+			// 尺寸适应；
+			window.addEventListener('resize',function(){
+				_this.parentWid = _this.targetDom.offsetWidth;
+				_this.childWid = parseFloat(_this.parentWid) * parseFloat(_this.scaleW);
+				for(var i = 0; i < _this.childDom.length; i ++){
+					_this.childDom[i].style.width = _this.childWid + 'px';
+				};
+			});
+			// 索引；
+			function index(current,obj){
+				for(var i = 0; i < obj.length; i ++){
+					if(obj[i] == current){
+						return i;
+					}
+				}
+			};
+			// 翻页；
+			function ClickPage(direction){
+
+				var navLength = _this.childDom.length;
+				var iAll = _this.targetDom.querySelectorAll('i');
+				var now = index(_this.targetDom.querySelector('.now'),iAll);
+				var max,min,changeNow = 0;
+				if(direction == 'prev'){
+					changeNow = -1;
+					min = 0;
+					max = navLength ;
+				}
+				else if(direction == 'next'){
+					changeNow = 1;
+					min = -1;
+					max = navLength - 1;
+				}
+				if((now > min) && (now < max)){
+					now += changeNow;
+					for(var i = 0; i < iAll.length; i++){
+						iAll[i].className = " ";
+					}
+					iAll[now].className = 'now';
+				}
+				_this.targetDom.querySelector('.scroll').cssText = 'marginLeft:-' + (now * _this.childWid) + 'px;transition:all 0.3s ease 0s;'
+			};
+			_this.prevBtn.addEventListener('click',function(){
+				ClickPage('prev');	
+			});
+			_this.nextBtn.addEventListener('click',function(){
+				ClickPage('next');
+			});
+		}
+	}
+	window.FunPage = FunPage;
+}(window,document));
+
+
